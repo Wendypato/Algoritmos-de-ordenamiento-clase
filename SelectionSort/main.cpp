8506f41d105e681a9a2a82f031aec0e95c6ecb4d
Wendy <152311301+Wendypato@users.noreply.github.com>
2024-10-30 17:08:56 -0600

    Update main.cpp

--- SelectionSort/main.cpp
+++ SelectionSort/main.cpp
@@ -3,34 +3,45 @@
 
 using namespace std;
 
-void bubbleSort(vector<int> & vec)
-{
+void selectionSort(vector<int>& vec) {
     int n = vec.size();
-    for(int i=0; i < n; i++){
-        for(int j =0;  j< n-1-i; j++)
-        {
-            if(vec[j] > vec[j+1])
-            {
-                int temp = vec[j];  //le podemos ponar std::swap(vec[j], vec[j+1]); Algo de entregar la funcion de swap
-                vec[j] = vec[j+1];
-                vec[j+1] = temp;
+
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i; //Almacenamos el indice del elemento minimo encontrado
+
+        //Buscamos el elemento minimo en el subarreglo no ordenado
+        for (int j = i + 1; j < n; j++) {
+            if (vec[j] < vec[minIndex]) {
+                minIndex = j; //Actualiza el indice del minimo
             }
         }
-    }
 
+
+        if (minIndex != i) {
+            int temp = vec[i];
+            vec[i] = vec[minIndex];
+            vec[minIndex] = temp;
+        }
+    }
 }
 
-int main()
-{
-
-    vector<int> vec = {1,3,7,4,2};
-
-    bubbleSort(vec);
-
-    for(int num : vec)
-    {
-        cout<<num;
+void printArray(const vector<int>& vec) {
+    for (int val : vec) {
+        cout << val << " ";
     }
+    cout << endl;
+}
+
+int main() {
+    vector<int> vec = {64, 25, 12, 22, 11};
+
+    cout << "Array original: ";
+    printArray(vec);
+
+    selectionSort(vec);
+
+    cout << "Array ordenado: ";
+    printArray(vec);
 
     return 0;
 }
